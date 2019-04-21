c86bb5f02bfb90c599d985c88233e24bb75a3b2a
Anny Guttierrez <agutilop@everis.com>
2019-04-21 01:01:12 -0500

    Update splash styles
    - Update splash styles.

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import React, {Component} from 'react';
 import {ImageBackground, StyleSheet, Text, View, Image, Dimensions} from 'react-native';
 
-const { width, height } = Dimensions.get('window');
 export default class App extends Component {
   render() {
     return (
-      <ImageBackground imageStyle={{ height }} style={{ flex: 1 }} source={require('./src/assets/background.png')}>
+      <ImageBackground imageStyle={{ resizeMode: 'cover' }} style={{ flex: 1 }} source={require('./src/assets/background.png')}>
       </ImageBackground>
     );
   }
